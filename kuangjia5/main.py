33b4f911bcf1f03712fbdad6292267149acfbb6a
xujunhao214 <2535581358@qq.com>
2025-06-10 09:33:56 +0800

    新增kuangjia6用例文件

---
+++ kuangjia5/main.py
@@ -0,0 +1,13 @@
+import os
+
+import pytest
+
+# # 启动测试框架
+# pytest.main(['./tests/test_api.py', '--alluredir', 'report/results', '--clean-alluredir'])
+# os.system('allure generate report/results -o report/report-allure --clean')
+
+# 启动测试框架
+pytest.main()
+
+os.system('allure generate -o report .allure_results --clean')
+   
