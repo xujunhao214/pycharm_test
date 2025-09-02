34d26ec90e2aefe13b92a5483201df17704bb300
xujunhao214 <2535581358@qq.com>
2025-09-02 11:47:28 +0800

    T

--- self_developed_model/VAR/VAR.py
+++ self_developed_model/VAR/VAR.py
@@ -8,7 +8,7 @@ PASSWORD = "Test123456"
 WEBHOOK_URL = "https://open.feishu.cn/open-apis/bot/v2/hook/8d3475ac-8adc-45ed-97c7-0f0ec8647a4f"
 
 # Jenkins路径
-JENKINS = "http://39.108.0.84:8080/job/Documentatio_Test/"
+JENKINS = "http://39.108.0.84:8080/job/Documentatio_model/"
 JENKINS_USERNAME = "admin"
 JENKINS_PASSWORD = "admin"
 
