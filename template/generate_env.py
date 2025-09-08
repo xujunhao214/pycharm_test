87350aa5a18e5ff902a419782421d0361a6709fe
xujunhao214 <2535581358@qq.com>
2025-09-08 18:10:16 +0800

    T

--- template/generate_env.py
+++ template/generate_env.py
@@ -20,7 +20,7 @@ ENV_CONFIG = {
     Environment.TEST.value: {
         "test_environment": "测试环境",
         "browser_version": "自研跟单1.4.2",
-        "base_url": "http://39.99.136.49:9000",
+        "base_url": "https://uat.atcp.top/api",
         "vps_url": "http://39.99.136.49:9001",
         "db_host": "39.99.136.49",
         "db_port": 3306,
