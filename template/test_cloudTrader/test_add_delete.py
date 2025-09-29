223a138bd70052fbee405bb404954c295d5a2216
xujunhao214 <2535581358@qq.com>
2025-09-29 09:03:20 +0800

    T

--- template/test_cloudTrader/test_add_delete.py
+++ template/test_cloudTrader/test_add_delete.py
@@ -23,7 +23,7 @@ def get_follow_accounts_from_runtime_json():
     # 2. 读取统一JSON文件（runtime_vars_cloud.json）
     json_file_path = os.path.join(
         os.path.dirname(__file__),  # 当前脚本目录
-        "../VAR/runtime_vars_cloud.json"  # 相对于当前脚本的JSON路径，需根据实际项目调整
+        "./VAR/runtime_vars_cloud.json"  # 相对于当前脚本的JSON路径，需根据实际项目调整
     )
     # 若当前脚本路径与VAR目录层级不同，可直接写绝对路径（如"D:/pycharm_test/template/VAR/runtime_vars_cloud.json"）
 
