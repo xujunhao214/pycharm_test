fbd9f4cac27ec1af194460751ebbcf5282c257e5
xujunhao214 <2535581358@qq.com>
2025-10-27 13:52:09 +0800

    T

--- lingkuan_1024/test_vps/test_create.py
+++ lingkuan_1024/test_vps/test_create.py
@@ -14,7 +14,6 @@ SKIP_REASON = "跳过此用例"
 @allure.feature("数据管理-创建数据-为VPS测试准备")
 class TestCreate(APITestBase):
     # @pytest.mark.skip(reason=SKIP_REASON)
-    @pytest.mark.retry(n=3, delay=5)
     @allure.title("账号管理-账号列表-新增单个用户")
     def test_create_user(self, logged_session, var_manager, encrypted_password):
         # 1. 发送创建用户请求
@@ -51,6 +50,7 @@ class TestCreate(APITestBase):
         )
 
     # @pytest.mark.skip(reason=SKIP_REASON)
+    @pytest.mark.retry(n=3, delay=5)
     @allure.title("数据库校验-账号列表-新增用户")
     def test_dbquery_user(self, var_manager, db_transaction):
         with allure.step("1. 查询数据库验证是否新增成功"):
