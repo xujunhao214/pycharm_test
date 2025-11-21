4034166ba6817f13a0d13dfc6345074a54b95a07
xujunhao214 <2535581358@qq.com>
2025-11-21 17:40:39 +0800

    T

--- lingkuanMT5_1027/run_vps_tests.py
+++ lingkuanMT5_1027/run_vps_tests.py
@@ -50,8 +50,6 @@ def run_vps_tests(env: str = "test"):
         # "test_vps/test_vps_masOrderClose.py",
         # "test_vps/test_vps_ordersenderror.py",
         # "test_vps/test_vpsOrder_open_level.py",
-        # "test_vps/test_vps_query.py",
-        # "test_vps/test_history_query.py",
         # "test_vps/test_vpsfixed_annotations.py",
         "test_vps/test_create_scene.py",
         # "test_vps/test_vpsMasOrder_money_scene.py",
