f8c81a8869f54a421387c19bb6b783e9cc08a2fb
xujunhao214 <2535581358@qq.com>
2025-12-03 11:55:51 +0800

    T

--- lingkuan_1127/test_vps/ianxil.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# 替换为你实际的文件路径
-file_path = "../report/vps_results/time_record.json"
-with open(file_path, "r", encoding="utf-8") as f:
-    data = json.load(f)
-    # 打印所有记录的用例方法和耗时
-    for item in data:
-        print(f"用例方法：{item['case_method']}，耗时：{item['elapsed_time']}ms")
